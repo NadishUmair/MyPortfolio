177b1cec8059eb0170721eab6d92b2e351a02c4e
Nadish Umair <nadishumair403@gmail.com>
2024-07-19 01:48:47 +0500

    save changes

--- app/components/Services/page.jsx
+++ app/components/Services/page.jsx
@@ -3,18 +3,14 @@ import React from "react";
 
 export default function Services() {
   return (
-    <div className="border h-[60vh]">
-      Services Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed
-      iusto impedit ipsam necessitatibus officiis. Illum, labore qui? Tenetur ad
-      dolore ipsa harum, amet eum cupiditate. Consequuntur illum labore totam
-      quis, iste eveniet at non commodi vitae cupiditate a incidunt facilis
-      reiciendis quasi, aliquid, placeat iusto ducimus ratione sapiente. Ipsam
-      consequuntur officia, inventore iure excepturi vel quod, exercitationem
-      ducimus, quisquam velit reprehenderit a. Expedita consequatur officiis
-      quae optio quod sed facere, vel, itaque deserunt sint qui eum, quibusdam
-      maiores mollitia laboriosam placeat! Aspernatur laborum ipsam nulla,
-      similique tempore dolorem velit cumque incidunt ex sapiente. Dignissimos
-      quibusdam quo asperiores error harum. Perspiciatis.
+    <div className="border h-[60vh] p-2">
+     <div className="flex flex-col justify-center items-center">
+      <h1 className="text-center text-2xl font-bold ">Services</h1>
+      <hr className="w-[20%] border-t-4 mt-2 border-[#61D2B4] " />
+     </div>
+     <div>
+      under devlope
+     </div>
     </div>
   );
 }
