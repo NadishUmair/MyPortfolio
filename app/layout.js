0438122822a6e03a3fd2bd4632bf9a190677b640
NadishUmair <nadishumair403@gmail.com>
2024-10-25 01:25:41 +0500

    save changes

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Nadish Umair- Software Developer",
   description:
-    "This is the portfolio of Hammad Ahmad. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Nadish Umair. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
