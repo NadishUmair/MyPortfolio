1e34cd9a88eb6f8f2bfd466a7cdecbce570956f0
NadishUmair <nadishumair403@gmail.com>
2024-11-11 22:10:54 +0500

    save changes

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -6,13 +6,19 @@ export const educations = [
     institution: "University of Management & Technology, Lahore",
   },
   {
-    id: 2,
+    id: 1,
+    title: "Full Stack Web Development",
+    duration: "2023 - 2024",
+    institution: "Ideoversity Training Excellence, Lahore",
+  },
+  {
+    id: 3,
     title: "Intermediate in computer science",
     duration: "2018 - 2020",
     institution: "Aspire Group of College, Pattoki",
   },
   {
-    id: 3,
+    id: 4,
     title: "Secondary School Certificate",
     duration: "2016 - 2018",
     institution: "First Nation School, Pattoki",
