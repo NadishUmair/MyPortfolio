022b8e4e29fd57e7aa39c183380366c1a3f4207b
NadishUmair <nadishumair403@gmail.com>
2025-04-16 14:26:56 +0500

    save

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,13 +3,14 @@ export const experiences = [
     id: 1,
     title: 'Junior Backend Developer',
     company: "Geeks Logicity llc",
-    duration: "(July 2024 - Present)"
+    duration: "(Feb 2024 - July 2024)"
+   
   },
   {
     id: 2,
-    title: "Full-Stack Developer",
+    title: "MERN Stack Developer",
     company: "Markedge (Pvt) ltd",
-    duration: "(Feb 2024 - Apr 2024)"
+     duration: "(Aug 2024 - Present)"
   },
  
 ]
